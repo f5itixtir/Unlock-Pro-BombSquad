6010c94986a7369e0b70d49e1f40f662f4eea499
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2019-02-02 12:35:36 +0530

    Add files via upload
    This Mod will help to get Unlock BombSquad Pro and Pro options

---
+++ UnlockPro.py
@@ -0,0 +1,10 @@
+#modBySobyDamn
+from bsUtils import *
+import bsUtils
+bs.screenMessage("Pro Unlocked By SobyDamn")
+def _000OOO0():
+    return True
+def _00000OOO():
+    return True
+bsUtils._haveProOptions = _000OOO0
+bsUtils._havePro = _00000OOO
